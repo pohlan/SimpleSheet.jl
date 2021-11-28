284cbd77067e75847b9f36eaa726245dfd7e2540
Annegret Pohle <apohle@ethz.ch>
2021-11-28 23:34:06 +0100

    refined plots

--- plot_comparisons.jl
+++ plot_comparisons.jl
@@ -1,4 +1,4 @@
-using JLD2, DataFrames, PyPlot, LaTeXStrings
+using JLD2, DataFrames, PyPlot, LaTeXStrings, LinearAlgebra
 
 # get SimpleSheet model results
 if !isfile("comparison.jld2")
@@ -13,31 +13,13 @@ nx, ny = 64, 32
 xs     = LinRange(0, 100e3, nx-2)
 ϕ_ref = get_ϕ_ref(xs)
 h_ref = get_h_ref(xs)
+rms_ϕ_ref = norm(ϕ_ref) / sqrt(length(ϕ_ref))
+rms_h_ref = norm(h_ref) / sqrt(length(h_ref))
+
+rm_points(A, n) = A[1:end-n]
 
 rcParams = PyPlot.PyDict(PyPlot.matplotlib."rcParams")
-rcParams["font.size"] = 18
-
-function get_marker(kwargs::NamedTuple)
-    if kwargs == (set_h_bc = false, e_v_num = 0.0, dt = 0.001)
-        sym   = "-o"
-        label = "standard"
-    elseif kwargs == (set_h_bc = true, e_v_num = 0.1, dt = 10)
-        sym = "-s"
-        label = "h b.c.; e_v_num"
-    end
-    return sym
-end
-
-function get_lab(kwargs::NamedTuple)
-    if kwargs.set_h_bc == false && kwargs.e_v_num == 0 && haskey(kwargs, update_h_only) ? kwargs.update_h_only == false : true
-        label = "standard"
-    else
-        label = string(kwargs)
-    end
-    return label
-end
-
-markers = ["-o", "-s", "-+", "-x", "-d"]
+rcParams["font.size"] = 24
 
 L2D = PyPlot.matplotlib.lines.Line2D
 methods = [ "RK4",
@@ -47,52 +29,69 @@ methods = [ "RK4",
             "RK4_piccard_loop",
             "GlaDS"]
 
-line_styles = Dict( "RK4"               => L2D([0], [0], color="green",   lw=2.2),
-                    "pseudo-transient"  => L2D([0], [0], color="orange",  lw=2.2),
-                    "diffeq_explicit"   => L2D([0], [0], color="blue",    lw=2.2),
-                    "forward-euler_CN"  => L2D([0], [0], color="magenta", lw=2.2),
-                    "RK4_piccard_loop"  => L2D([0], [0], color="cyan",    lw=2.2),
+line_styles = Dict( "RK4"               => L2D([0], [0], color="cadetblue",      lw=3),
+                    "pseudo-transient"  => L2D([0], [0], color="orange",         lw=3),
+                    "diffeq_explicit"   => L2D([0], [0], color="indianred",      lw=3),
+                    "forward-euler_CN"  => L2D([0], [0], color="hotpink",        lw=3),
+                    "RK4_piccard_loop"  => L2D([0], [0], color="deepskyblue", lw=3),
                     "GlaDS"             => L2D([0], [0], color="black",   lw=3  ))
 
-fig, axs = subplots(1, 2, figsize=(15,10))
-fig.subplots_adjust(hspace=0.3, wspace=0.3)
+kw_lines    = Dict( "standard"          => L2D([0], [0], color="orange", lw=2.2, ls="-",  marker="o"),
+                    "e_v_num"           => L2D([0], [0], color="orange", lw=2.2, ls=":",  marker="o"),
+                    "split-step"        => L2D([0], [0], color="orange", lw=2.2, ls="--", marker="o"))
+
+function get_style(method, kws)
+    if method == "pseudo-transient"
+        if kws.e_v_num > 0
+            style = join([kw_lines["e_v_num"].get_ls(), kw_lines["e_v_num"].get_marker()])
+        elseif kws.update_h_only == true
+            style = join([kw_lines["split-step"].get_ls(), kw_lines["e_v_num"].get_marker()])
+        else
+            style = join([kw_lines["standard"].get_ls(), kw_lines["e_v_num"].get_marker()])
+        end
+    else
+        style = "-o"
+    end
+    return style
+end
+
+fig, axs = subplots(1, 2, figsize=(18,10))
+fig.subplots_adjust(hspace=0.3, wspace=0.5)
 for method in methods
     i = findall(df.method .== method)
     col = line_styles[method].get_color()
     lw  = line_styles[method].get_lw()
 
-    # axs[1].plot.(df.nit[i], df.rms_ϕ[i], markers[1], color=col, lw=lw, label=method)
-    # axs[1].set_xlabel("# iterations")
-    # axs[1].set_ylabel("RMS(ϕ_test - ϕ_ref)")
-    # axs[1].set_yscale("log")
-    # axs[1].legend(custom_legend, methods)
-    # axs[1].set_title(L"ϕ")
+    if method == "forward-euler_CN" || method == "RK4"
+        axs[1].plot.(rm_points.(df.t_run[i], 2)./60, rm_points.(df.rms_ϕ[i], 2) ./ rms_ϕ_ref, get_style(method, df.kwargs), color=col, lw=lw, label=method)
+    else
+        axs[1].plot.(df.t_run[i]./60, df.rms_ϕ[i] ./ rms_ϕ_ref, get_style.(method, df.kwargs[i]), color=col, lw=lw, label=method)
+    end
 
-    axs[1].plot.(df.t_run[i]./60, df.rms_ϕ[i], markers[1], color=col, lw=lw, label=method)
-    axs[1].set_xscale("log")
+    # axs[1].set_xscale("log")
     axs[1].set_yscale("log")
     axs[1].set_xlabel("Wall time (min)")
-    axs[1].set_ylabel("RMS(ϕ_test - ϕ_ref)")
-    #axs[1].set_title(L"ϕ")
+    axs[1].set_ylabel(L"$\mathrm{RMS}_ϕ$ / $\mathrm{RMS}_{ϕ_\mathrm{ref}}$") # RMS($ϕ_\text{test}$ - $ϕ_\text{ref}$)
+    axs[1].set_title(L"ϕ")
 
-    axs[1].legend([line_styles[m] for m in methods], methods)
+    axs[1].legend([kw_lines[m] for m in keys(kw_lines)], collect(keys(kw_lines)),
+                  title="Pseudo-transient",
+                  title_fontsize="small",
+                  fontsize="small",
+                  loc=(0.5, 0.2))
 
-    # axs[3].plot.(df.nit[i], df.rms_h[i], markers[1], color=colors[method])
-    # axs[3].set_xlabel("# iterations")
-    # axs[3].set_ylabel("RMS(h_test - h_ref)")
-    # axs[3].set_yscale("log")
-    # axs[3].set_title(L"h")
-
-
-    axs[2].plot.(df.t_run[i]./60, df.rms_h[i], markers[1], color=col)
-    axs[2].set_xscale("log")
+    axs[2].plot.(df.t_run[i]./60, df.rms_h[i] ./ rms_h_ref, get_style.(method, df.kwargs[i]), color=col, lw=lw)
+    # axs[2].set_xscale("log")
     axs[2].set_yscale("log")
     axs[2].set_xlabel("Wall time (min)")
-    axs[2].set_ylabel("RMS(h_test - h_ref)")
-    #axs[2].set_title(L"h")
+    axs[2].set_ylabel(L"$\mathrm{RMS}_h$ / $\mathrm{RMS}_{h_\mathrm{ref}}$")
+    axs[2].set_title(L"h")
+
+    axs[2].legend([line_styles[m] for m in methods], methods[1:end-1],
+                   fontsize="small")
 end
 
-fig, axs = subplots(1, 2, figsize=(15,10))
+fig, axs = subplots(1, 2, figsize=(18,10))
 fig.subplots_adjust(hspace=0.3, wspace=0.3)
 for method in methods
     i = findall(df.method .== method)
@@ -107,9 +106,8 @@ for method in methods
         axs[1].plot.(repeat([xs .* 1e-3], length(i)), ϕ_test, color=col, lw=lw, label=method)
     end
     axs[1].set_xlabel("x (km)")
-    #axs[1].set_ylabel("ϕ")
-    axs[1].legend([line_styles[m] for m in methods], methods)
-    axs[1].set_title("ϕ (Pa)")
+    axs[1].set_ylabel("ϕ (Pa)")
+    #axs[1].set_title("ϕ (Pa)")
 
     if method == "GlaDS"
         axs[2].plot(xs .* 1e-3, h_ref, color="black", lw=3, label="GlaDS")
@@ -117,7 +115,12 @@ for method in methods
         axs[2].plot.(repeat([xs .* 1e-3], length(i)), h_test, color=col, lw=lw, label=method)
     end
     axs[2].set_xlabel("x (km)")
-    axs[2].set_title("h (m)")
+    axs[2].set_ylabel("h (m)")
+    #axs[2].set_title("h (m)")
+
+    axs[2].legend([line_styles[m] for m in methods], methods, fontsize="small")
+
+    fig.suptitle("Fields after maximum wall time")
 end
 
 # all variants of one method
